e5cf927455101df0dcdc9d8075b5aae5c004d1cf
Sandeep <sandeepsk15892>
2022-05-02 12:21:41 +0530

    navigation setup

---
+++ app/src/main/java/sk/sandeep/marsphotoapp/overview/OverviewFragment.kt
@@ -0,0 +1,20 @@
+package sk.sandeep.marsphotoapp.overview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import sk.sandeep.marsphotoapp.R
+
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+}
