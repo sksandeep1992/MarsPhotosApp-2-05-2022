a8bc8c3b9096e0b077e821a30d64105fea557bd8
Sandeep <sandeepsk15892>
2022-05-02 12:32:15 +0530

    retrofit setting

---
+++ app/src/main/java/sk/sandeep/marsphotoapp/network/MarsApiService.kt
@@ -0,0 +1,23 @@
+
+package sk.sandeep.marsphotoapp.network
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos() :String
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java) }
+}
